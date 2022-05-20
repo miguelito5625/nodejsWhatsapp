98cd169933cfb26885d02d2fbd3e959bc846aab0
exbinario <miguel.4to.1994@gmail.com>
2022-05-19 18:43:22 -0600

    test3

--- whaserver.js
+++ whaserver.js
@@ -5,7 +5,10 @@ const { Client } = require('whatsapp-web.js');
 const app = express();
 const port = 3000;
 
-const client = new Client({ puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']} });
+const client = new Client({ 
+    authTimeoutMs: 900000,
+    puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']} 
+});
 // const client = new Client();
 
 client.on('qr', qr => {
